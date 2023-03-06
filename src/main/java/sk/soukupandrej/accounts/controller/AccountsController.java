3485552116d431796800f81b7b0bc336a7d6cfea
soukupa <soukup.andrej@gmail.com>
2023-03-06 21:57:08 +0100

    initial commit 2

--- src/main/java/sk/soukupandrej/accounts/controller/AccountsController.java
+++ src/main/java/sk/soukupandrej/accounts/controller/AccountsController.java
@@ -1,20 +1,36 @@
 package sk.soukupandrej.accounts.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sk.soukupandrej.accounts.config.AccountsServiceConfig;
 import sk.soukupandrej.accounts.model.Accounts;
 import sk.soukupandrej.accounts.model.Customer;
+import sk.soukupandrej.accounts.model.Properties;
 import sk.soukupandrej.accounts.repository.AccountsRepository;
 
 @RestController
 public class AccountsController {
     @Autowired
     private AccountsRepository accountsRepository;
+    @Autowired
+    private AccountsServiceConfig accountsConfig;
 
     @PostMapping("/myAccount")
     public Accounts getAccountDetail(@RequestBody Customer customer){
         return accountsRepository.findByCustomerId(customer.getCustomerId());
     }
-}
+
+    @GetMapping("/account/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(accountsConfig.getMsg(), accountsConfig.getBuildVersion(), accountsConfig.getMailDetails(),
+                accountsConfig.getActiveBranches());
+        return ow.writeValueAsString(properties);
+    }
+ }
