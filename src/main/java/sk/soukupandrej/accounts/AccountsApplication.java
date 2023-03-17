dce198493a7718a424f65876fb1ec180aa3a01fe
soukupa <soukup.andrej@gmail.com>
2023-03-17 22:05:21 +0100

    refreshScope + start of eureka

--- src/main/java/sk/soukupandrej/accounts/AccountsApplication.java
+++ src/main/java/sk/soukupandrej/accounts/AccountsApplication.java
@@ -2,11 +2,13 @@ package sk.soukupandrej.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 //@ComponentScans({@ComponentScan("sk.soukupandrej.accounts.controller")})
 //@EnableJpaRepositories("sk.soukupandrej.accounts.repository")
 //@EntityScan("sk.soukupandrej.accounts.model")
+@RefreshScope
 public class AccountsApplication {
 
     public static void main(String[] args) {
